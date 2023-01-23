ba669b12cf5f246b494565566ed9afd59f4d1d1e
maruzibo <marx1987@mail.ru>
2023-01-23 12:17:16 +0100

    files modifies

--- first_word.c
+++ first_word.c
@@ -1,32 +1,13 @@
 #include<unistd.h>
 /*
-Assignment name  : first_word
-Expected files   : first_word.c
-Allowed functions: write
---------------------------------------------------------------------------------
-
-Write a program that takes a string and displays its first word, followed by a
+This program takes a string and displays its first word, followed by a
 newline.
 
-A word is a section of string delimited by spaces/tabs or by the start/end of
+Here were by a word means a section of a string bounded by spaces and/or tabs or by the beginning or the end of
 the string.
 
-If the number of parameters is not 1, or if there are no words, simply display
+In case the number of parameters is not 1, or if there are no words, the program should display
 a newline.
-
-Examples:
-
-$> ./first_word "FOR PONY" | cat -e
-FOR$
-$> ./first_word "this        ...       is sparta, then again, maybe    not" | cat -e
-this$
-$> ./first_word "   " | cat -e
-$
-$> ./first_word "a" "b" | cat -e
-$
-$> ./first_word "  lorem,ipsum  " | cat -e
-lorem,ipsum$
-$>
 */
 int main(int c, char  **v)
 {
